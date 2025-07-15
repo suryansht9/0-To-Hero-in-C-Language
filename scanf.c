c68f7dcecde9853a62429c33968dc128a953dc81
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-07-15 22:00:14 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    printf("hey user please enetr the value of a\n");
+    scanf("%d", &a);
+    printf("hey user now please enter the value of b\n");
+    scanf("%d", &b);
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %d\n", b);
+    
+    return 0;
+
+
+}
