26396a8c74ba9136dbe73e9dc88bc45e40968a9f
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-07-16 21:53:10 +0530

    Add files via upload

---
+++ arithmetic_operator.c
@@ -0,0 +1,61 @@
+//addition of two numbers
+//This code is written in C11 standard
+#include<stdio.h>
+int main(){
+int a;
+int b;
+int n;
+printf("hey please enter the value of a\n");
+scanf("%d", &a);
+printf("hey now please enter the value of b\n");
+scanf("%d", &b);
+n =a + b;
+printf("the sum of a and b is %d",n);
+
+return 0;
+}
+
+//subtraction of two numbers
+//This code is written in C11 standard
+#include<stdio.h>
+int main(){
+int a;
+int b;
+int n;
+printf("hey please enter the value of a\n");
+scanf("%d", &a);
+printf("hey now please enter the value of b\n");
+scanf("%d", &b);
+n = a - b;
+printf("the difference of a and b is %d",n);
+return 0;
+}
+
+//multiplication of two numbers
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int n;
+    printf("hey please enter the value of a\n");
+    scanf("%d", &a);
+    printf("hey now please enter the value of b\n");
+    scanf("%d", &b);
+    n = a * b;
+    printf("the product of a and b is %d",n);
+    return 0;
+}
+
+//division of two numbers
+#include<stdio.h>
+int main(){
+    int a;
+    int b;
+    int c;
+    printf("hey please enter the value of a \n");
+    scanf("%d",&a);
+    printf("hey now please enter the value of b \n");
+    scanf("%d",&b);
+    c= a / b;
+    return 0;
+} 
