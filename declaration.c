23aa4b854327f6ac47ee788a534d6c790013ff3e
Suryansh tripathii <suryanshtripathi778@gmail.com>
2026-05-14 11:20:25 +0530

    Add files via upload

---
+++ declaration.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    int marks[5]; //this is the declaration of array
+    printf("the size of this array in stack memory is :%d\n",sizeof(marks));
+    //also we show the size of this array used in stack memory
+    return 0;
+}
