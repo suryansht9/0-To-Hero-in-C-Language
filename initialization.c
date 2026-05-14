2fb0062b257d014c37dc5bad022b8cdaa70498cc
Suryansh tripathii <suryanshtripathi778@gmail.com>
2026-05-14 11:19:41 +0530

    Add files via upload

---
+++ initialization.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5]={45,47,84,57,66};
+    printf("The marks of student 1 is :%d\n",arr[0]);
+    printf("The marks of student 2 is :%d\n",arr[1]);
+    printf("The marks of student 3 is :%d\n",arr[2]);
+    printf("The marks of student 4 is :%d\n",arr[3]);
+    printf("The marks of student 5 is :%d\n",arr[4]);
+    return 0;
+}
